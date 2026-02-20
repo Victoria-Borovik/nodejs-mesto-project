6819d010b1c9acb44e075e5d4afc9bcc3302ae2c
Victoria Kudriashova <borovik.va111@gmail.com>
2026-02-21 02:01:59 +0300

    Feat: add logger

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import mongoose from 'mongoose';
 import { rateLimit } from 'express-rate-limit';
 import dotenv from 'dotenv';
@@ -6,9 +6,11 @@ import cookieParser from 'cookie-parser';
 import { login, createUser } from './controllers/users';
 import userRoutes from './routes/users';
 import cardsRoutes from './routes/cards';
+import { requestLogger, errorLogger } from './middlewares/logger';
 import auth from './middlewares/auth';
 import handleError from './middlewares/handleError';
-import { NOT_FOUND_ERROR_CODE, errorText } from './constants';
+import NotFoundError from './errors/not-found-err';
+import { errorText } from './constants';
 
 dotenv.config();
 
@@ -23,19 +25,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(limiter);
 app.use(cookieParser());
+app.use(requestLogger);
 
 app.post('/signin', login);
 app.post('/signup', createUser);
 app.use(auth);
 app.use('/users', userRoutes);
 app.use('/cards', cardsRoutes);
-
-app.use((_, res) => {
-  res.status(NOT_FOUND_ERROR_CODE).json({
-    message: errorText.routeNotFound,
-  });
+app.use((_req: Request, _res: Response, next: NextFunction) => {
+  next(new NotFoundError(errorText.routeNotFound));
 });
 
+app.use(errorLogger);
 app.use(handleError);
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
